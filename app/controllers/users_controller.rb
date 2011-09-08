2f4b9f12489d74d51611bf7bb67e11cb582970b2
Joey <joey@ubuntu.(none)>
2011-09-08 11:43:28 -0500

    added code to demonstrate bug if params[:bug] == 'true'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,11 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
-
+    if params[:bug] == 'true'
+      @user = User.select(:name).find(params[:id])
+    else
+      @user = User.find(params[:id])
+    end
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
